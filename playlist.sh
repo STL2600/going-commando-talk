83c386815f93605d776cad67729a36700aafa0fb
Robert Ward <robert@rtward.com>
2017-10-30 19:48:48 -0500

    convert playlist to list each file

--- playlist.sh
+++ playlist.sh
@@ -1,15 +1,52 @@
 #!/bin/bash
 
-# Import list of Asciinema recording filenames.
-RECORDINGS=(`ls *.json`)
+read -p "Press any key to play the next recording" -n1 -s
+asciinema play 01-tmux.json
 
-clear
+read -p "Press any key to play the next recording" -n1 -s
+asciinema play 02-mutt.json
 
-for PLAY in ${RECORDINGS[*]}
-do
-	read -p "Press any key to play the next recording" -n1 -s
-    clear
-	asciinema play $PLAY 
+read -p "Press any key to play the next recording" -n1 -s
+asciinema play 03-bitlbee.json
 
-done
+read -p "Press any key to play the next recording" -n1 -s
+asciinema play 04-pandoc.json
 
+read -p "Press any key to play the next recording" -n1 -s
+asciinema play 05-sc.json
+
+read -p "Press any key to play the next recording" -n1 -s
+asciinema play 06-khal.json
+
+read -p "Press any key to play the next recording" -n1 -s
+asciinema play 07-web-browser.json
+
+read -p "Press any key to play the next recording" -n1 -s
+asciinema play 08-twitter.json
+
+read -p "Press any key to play the next recording" -n1 -s
+asciinema play 09-reddit.json
+
+read -p "Press any key to play the next recording" -n1 -s
+asciinema play 11-mps-youtube.json
+
+read -p "Press any key to play the next recording" -n1 -s
+asciinema play 12-newsboat.json
+
+read -p "Press any key to play the next recording" -n1 -s
+asciinema play 13-music.json
+
+read -p "Press any key to play the next recording" -n1 -s
+asciinema play 14-rtorrent.json
+
+read -p "Press any key to play the next recording" -n1 -s
+asciinema play 15-image-viewer.json
+
+read -p "Press any key to play the next recording" -n1 -s
+asciinema play 16-image-editing.json
+
+read -p "Press any key to play the next recording" -n1 -s
+asciinema play 17-video-player.json
+
+read -p "Press any key to play the next recording" -n1 -s
+asciinema play 18-video-editing.json
